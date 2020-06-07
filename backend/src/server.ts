b6940c5d788d58915d9c15db77c1480ecc8f42e4
sibelly <sibellychan@hotmail.com>
2020-06-07 18:46:55 -0400

    Adiciona validacao no backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import cors from 'cors'
-import routes from './routes'
 import path from 'path'
+import { errors } from 'celebrate'
+
+import routes from './routes'
 
 const app = express()
 
@@ -10,5 +12,7 @@ app.use(express.json())
 app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors())
 
 app.listen(3333)
